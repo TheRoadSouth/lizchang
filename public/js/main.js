35934785b41c3bbd2d522ecca377074c7cc6ddb2
Martin Przybyla <mprzybyla@ancestry.com>
2015-12-02 23:35:15 -0800

    fixes active links

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
 (function(){
-  console.log("hola");
+  var currPath = window.location.pathname.split('/')[1],
+    navElems = document.querySelectorAll(".nav-link"),
+    UILink = document.getElementById('UI'),
+    GraphicLink = document.getElementById('Graphic'),
+    AboutLink = document.getElementById('About');
+
+  [].forEach.call(navElems, function(el) {
+    el.classList.remove("active");
+  });
+
+  (currPath === 'ui' || currPath === '') ?  UILink.className = "active" : "";
+  (currPath === 'graphic') ? GraphicLink.className = "active" : "";
+  (currPath === 'about') ? AboutLink.className = "active" : "";
 })()
