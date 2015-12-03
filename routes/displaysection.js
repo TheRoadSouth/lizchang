664073cd7b3d1b4487607aeed0873e5efdb1d4c1
Martin Przybyla <mprzybyla@ancestry.com>
2015-12-02 20:13:17 -0800

    added routes and about page

---
+++ routes/displaysection.js
@@ -0,0 +1,53 @@
+var displaySection = function(req, res, next, section, returnUrl) {
+  var fs = require('fs'),
+    obj,
+    projects,
+    id,
+    projTitle,
+    projDesc,
+    client,
+    responsibleFor,
+    url,
+    completedAt,
+    imageFullUrl,
+    imageHeight,
+    imageWidth;
+
+  fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    projects = obj.projects[section];
+
+    projects.forEach(function(idx) {
+      if (idx.slug === req.params.id) {
+        id = idx.slug;
+        projTitle = idx.title;
+        projDesc = idx.description;
+        client = idx.client;
+        category = idx.category;
+        responsibleFor = idx.responsibleFor;
+        url = idx.url;
+        completedAt = idx.completedAt;
+        completedDate = idx.completedDate;
+        imageFullUrl = idx.imageFullUrl;
+        imageHeight = idx.imageHeight;
+        imageWidth = idx.imageWidth;
+      }
+    });
+    res.render('project', {
+      title: projTitle,
+      projDesc: projDesc,
+      returnUrl: returnUrl,
+      imageHeight: imageHeight,
+      imageWidth: imageWidth,
+      client: client,
+      category: category,
+      responsibleFor: responsibleFor,
+      url: url,
+      completedAt: completedAt,
+      completedDate: completedDate,
+      imageFullUrl: imageFullUrl });
+  });
+}
+
+  module.exports = displaySection;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var displaySection = require('./displaySection');
 
 router.get('/', function(req, res, next) {
   var fs = require('fs'),
@@ -10,58 +11,37 @@ router.get('/', function(req, res, next) {
     if (err) throw err;
     obj = JSON.parse(data);
     projects = obj.projects.ui;
-    res.render('index', { title: 'UI', section: 'ui', projects: projects});
+    res.render('index', { title: 'UI', projects: projects});
   });
 });
 
 router.get('/ui/:id', function(req, res, next) {
+  var section = 'ui',
+      returnUrl = '/';
+    displaySection(req, res, next, section, returnUrl);
+});
+
+router.get('/graphic', function(req, res, next) {
   var fs = require('fs'),
     obj,
-    projects,
-    id,
-    projTitle,
-    projDesc,
-    client,
-    responsibleFor,
-    url,
-    completedAt,
-    imageFullUrl,
-    imageHeight,
-    imageWidth;
+    projects;
 
   fs.readFile('data.json', 'utf8', function (err, data) {
     if (err) throw err;
     obj = JSON.parse(data);
-    projects = obj.projects.ui;
-
-    projects.forEach(function(idx) {
-      if (idx.slug === req.params.id) {
-        id = idx.slug;
-        projTitle = idx.title;
-        projDesc = idx.description;
-        client = idx.client;
-        responsibleFor = idx.responsibleFor;
-        url = idx.url;
-        completedAt = idx.completedAt;
-        imageFullUrl = idx.imageFullUrl;
-        imageHeight = idx.imageHeight;
-        imageWidth = idx.imageWidth;
-      }
-    });
-    res.render('project', { title: projTitle, projDesc: projDesc, imageHeight: imageHeight, imageWidth: imageWidth, client: client, responsibleFor: responsibleFor, url: url, completedAt: completedAt, imageFullUrl: imageFullUrl });
+    projects = obj.projects.graphic;
+    res.render('index', { title: 'UI', projects: projects});
   });
 });
 
-router.get('/graphic', function(req, res, next) {
-  res.render('index', { title: 'Graphic' });
-});
-
 router.get('/graphic/:id', function(req, res, next) {
-  res.render('project', { title: 'Graphic', id: req.params.id });
+  var section = 'graphic',
+      returnUrl = '/graphic';
+    displaySection(req, res, next, section, returnUrl);
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
